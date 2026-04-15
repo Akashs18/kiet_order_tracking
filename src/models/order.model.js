0813535c1749d67915edf61987debc56c73381b8
akash <a8892242921@gmail.com>
2026-04-15 12:08:48 +0530

    delivery date added,supplier name fixed,email notification added

--- src/models/order.model.js
+++ src/models/order.model.js
@@ -1,10 +1,10 @@
 const db = require('../config/db');
 
-exports.create =(po,email) =>{
+exports.create =(po,supplierName,email,expectedDeliveryDate) =>{
     return db.query(
-        `insert into orders (po_number,client_email,status)
-        values ($1, $2, 'PENDING') returning *`,
-        [po,email]
+        `insert into orders (po_number,supplier_name,client_email,status,expected_delivery_date)
+        values ($1, $2, $3, 'PENDING', $4) returning *`,
+        [po,supplierName,email,expectedDeliveryDate]
     );
 };
 
@@ -15,9 +15,16 @@ exports.getByEmail =(email) => db.query('select * from orders where client_email
 exports.getById = (id) => db.query('select * from orders where id = $1', [id]);
 
 exports.updateStatus =(id,status,field)=>{
-    return db.query(
-        `update orders set status=$1, ${field}=current_timestamp where id=$2 returning *`,
-        [status,id]
-    );
+    if (field) {
+        return db.query(
+            `update orders set status=$1, ${field}=current_timestamp where id=$2 returning *`,
+            [status,id]
+        );
+    } else {
+        return db.query(
+            `update orders set status=$1 where id=$2 returning *`,
+            [status,id]
+        );
+    }
 };
 
